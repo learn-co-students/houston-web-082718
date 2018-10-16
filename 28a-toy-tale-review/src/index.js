0873753eac7e8e3e7b7ccdb2778323225a6759d0
Michael Cheng <cheng.c.mike@gmail.com>
2018-10-16 13:27:00 -0500

    finished project

--- 28a-toy-tale-review/src/index.js
+++ 28a-toy-tale-review/src/index.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
           <h2>${toy.name}</h2>
           <img src="${toy.image}" class="toy-avatar">
           <p>${toy.likes} Likes <p>
-          <button class="like-btn">Like <3</button>
+          <button class="like-btn" data-id="${toy.id}">Like <3</button>
         </div>
         `;
       });
@@ -105,9 +105,34 @@ document.addEventListener("DOMContentLoaded", function() {
       // });
     });
 
+  // Add global event listener (event delegation)
   document.addEventListener("click", function(event) {
+    // Only to respond to clicks of the like-btn
     if (event.target.className === "like-btn") {
-      ("blah");
+      // 1. Grab like count element
+      const likeCountElement = event.target.parentNode.parentNode.children[2];
+      // const likeCountElement = event.target.parentNode.previousElementSibling;
+
+      // 2. Get the number from the element
+      const likeCount = parseInt(likeCountElement.innerText);
+      const newLikeCount = likeCount + 1;
+
+      // 3. Increment the like count
+      likeCountElement.innerText = `${newLikeCount} likes`;
+
+      // 4. Make PATCH request
+      const id = event.target.dataset.id;
+
+      fetch(`http://localhost:3000/toys/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          likes: newLikeCount
+        })
+      });
     }
   });
 });
